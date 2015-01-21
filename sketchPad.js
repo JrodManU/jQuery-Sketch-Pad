a8edf78af901761f539b1772881d57d677ab4745
JrodManU <jrodmanu@yahoo.com>
2015-01-20 21:09:44 -0500

    Added more content

--- sketchPad.js
+++ sketchPad.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 
 		$("#container").empty();
 
+		var mouseDown = false;
 		var canvasSize = 600;
 		$("#container").width(canvasSize);
 		$("#container").height(canvasSize);
@@ -25,11 +26,39 @@ $(document).ready(function() {
 
 		}
 
-		//Color the boxes
+		//Sensor stuff that needs to be local
+
+		//Detect if mouse is down
+		$(".box").mousedown(function() {
+
+			mouseDown = true;
+			//Accounts for first box
+			color($(this));
+
+		});
+
+		$(".box").mouseup(function() {
+
+			mouseDown = false;
+
+		});
+
+		//Turn off if leaving the container
+		$("#container").mouseleave(function() {
+
+			mouseDown = false;
+
+		});
+
+		//Detect if entering new boxes
 		$(".box").mouseenter(function() {
 
-			$(this).css("background-color", "black");
-		
+			if(mouseDown) {
+
+				color($(this));
+
+			}
+			
 		});
 
 	}
@@ -43,5 +72,28 @@ $(document).ready(function() {
 		generate();
 
 	});
+
+	//Determines what to do with the box
+	function color(box) {
+
+		switch($('input[name="style"]:checked').val()) {
+
+			case "trail":
+				box.css("opacity", 0);
+				box.fadeTo(300, 1);
+				break;
+			case "fade":
+				box.css("opacity", box.css("opacity") - .1);
+				break;
+			default:
+				if(box.css("opacity") == 1) {
+					box.css("opacity", 0);
+				} else {
+					box.css("opacity", 1);
+				}
+				break;
+		}
+
+	}
 
 });
